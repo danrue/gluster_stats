8d7bb943d04cf635afe5491360ae6ad64d112687
Dan Rue <drue@spscommerce.com>
2016-06-08 14:06:20 -0500

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='gluster-stats',
-    version='0.1.1',
+    version='0.1.2',
     description=('Display gluster stats'),
     long_description=readme + '\n\n' + history,
     author="Dan Rue",
