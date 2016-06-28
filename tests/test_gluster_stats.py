71b898827654d5773203e918db414b03d01ea2cc
Dan Rue <drue@therub.org>
2016-06-28 12:57:51 -0500

    Abstract testing
    Now supports dropping in matching *_in.json and *_out.json files.

--- tests/test_gluster_stats.py
+++ tests/test_gluster_stats.py
@@ -7,6 +7,7 @@ test_gluster_stats
 
 Tests for `gluster_stats` module.
 """
+import glob
 import hashlib
 import json
 import os
@@ -18,19 +19,27 @@ from builtins import object
 class TestGlusterStats(object):
     """ Test gluster-stats """
 
-    def test_375_default(self):
+    def test_default(self):
         """ Normal Run """
-        stats = gluster_stats.GlusterStats(
-                  test_file="tests/test_sources/gluster_stats_3.7.5_in.json")
-        output = stats.get_stats()
-        del output['generated_timestamp'] # Remove dynamic keys
-        del output['gluster_stats_version'] # Remove dynamic keys
 
-        #print(json.dumps(output, indent=1, sort_keys=True))
+        ins = glob.glob("tests/test_sources/gluster_stats*_in.json")
+        outs = glob.glob("tests/test_sources/gluster_stats*_out.json")
+        for in_file in ins:
+            out_file = in_file.replace("_in.json", "_out.json")
+            assert out_file in outs
 
-        with open('tests/test_sources/gluster_stats_3.7.5_out.json') as f:    
-            expected_out = json.load(f)
-        del expected_out['generated_timestamp'] # Remove dynamic keys
-        del expected_out['gluster_stats_version'] # Remove dynamic keys
+            print("Testing {0}".format(in_file))
 
-        assert output == expected_out
+            stats = gluster_stats.GlusterStats(test_file=in_file)
+            output = stats.get_stats()
+            del output['generated_timestamp'] # Remove dynamic keys
+            del output['gluster_stats_version'] # Remove dynamic keys
+
+            #print(json.dumps(output, indent=1, sort_keys=True))
+
+            with open(out_file) as f:
+                expected_out = json.load(f)
+            del expected_out['generated_timestamp'] # Remove dynamic keys
+            del expected_out['gluster_stats_version'] # Remove dynamic keys
+
+            assert output == expected_out
